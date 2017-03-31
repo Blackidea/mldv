c150bf6ae4de58eaf8eab9105aba42821d31e36e
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-03-31 09:09:38 +0300

    add task videos

--- gulpfile.js/tasks/build-production.js
+++ gulpfile.js/tasks/build-production.js
@@ -2,5 +2,5 @@ var gulp         = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
 gulp.task('build:production', function(cb) {
-  gulpSequence('clean', ['fonts', 'images'], 'jade', ['stylus', 'webpack:production'], 'rev', 'vendor-js', cb);
+  gulpSequence('clean', ['fonts', 'images', 'videos'], 'jade', ['stylus', 'webpack:production'], 'rev', 'vendor-js', cb);
 });
--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -3,11 +3,13 @@ var jade      = require('../config/jade');
 var images    = require('../config/images');
 var stylus    = require('../config/stylus');
 var fonts     = require('../config/fonts');
+var videos    = require('../config/videos');
 var watch     = require('gulp-watch');
 
 gulp.task('watch', ['browserSync'], function() {
   watch(images.src, function() { gulp.start('images'); });
   watch(stylus.src, function() { gulp.start('stylus'); });
   watch(fonts.src, function() { gulp.start('fonts'); });
+  watch(videos.src, function() { gulp.start('videos'); });
   watch(jade.watch, function() { gulp.start('jade'); });
 });
